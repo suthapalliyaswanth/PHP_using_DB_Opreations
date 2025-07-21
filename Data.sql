68326b51e248f9eff2f930231f2a72e1e30dd309
suthapalliyaswanth <136904864+suthapalliyaswanth@users.noreply.github.com>
2025-07-21 23:26:45 +0530

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,50 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 16, 2022 at 08:58 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `db_crud_example`
---
 
--- --------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `db_crud_example`;
+USE `db_crud_example`;
 
---
--- Table structure for table `mahasiswa`
---
-
-CREATE TABLE `mahasiswa` (
-  `nim` int(12) NOT NULL,
-  `nama_lengkap` varchar(150) NOT NULL,
-  `alamat` text NOT NULL,
-  `jurusan` varchar(150) NOT NULL
+-- Table structure for table `emp`
+CREATE TABLE IF NOT EXISTS `emp` (
+  `nim` INT(12) NOT NULL,
+  `nama_lengkap` VARCHAR(150) NOT NULL,
+  `alamat` TEXT NOT NULL,
+  `jurusan` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`nim`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`nim`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
